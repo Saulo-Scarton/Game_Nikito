832e35ee4509bbff2df24163851a4f7efe7de300
Saulo-Scarton <82216650+Saulo-Scarton@users.noreply.github.com>
2021-09-02 15:35:26 -0300

    att
    adjust flower

--- src/com/starton/entities/Flower.java
+++ src/com/starton/entities/Flower.java
@@ -8,7 +8,7 @@ import com.starton.world.Camera;
 
 public class Flower extends Entity{
 
-	private int frames = 0,maxFrames = 25 /*max frames para reduzir a velocidade da animação do inimigo*/,index = 0,maxIndex = 5;
+	private int frames = 0,maxFrames = 30 /*max frames para reduzir a velocidade da animação do inimigo*/,index = 0,maxIndex = 5;
 	
 	private BufferedImage[] sprites;
 	
@@ -16,9 +16,15 @@ public class Flower extends Entity{
 	public Flower(int x, int y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
 		sprites = new BufferedImage[6];
-		sprites[0] = Game.spritesheet.getSprite(0, 64, 16, 16);
-		sprites[1] = Game.spritesheet.getSprite(32, 64, 16, 16);
-		sprites[2] = Game.spritesheet.getSprite(16, 64, 16, 16);
+		sprites[0] = Game.spritesheet.getSprite(0, 64, 16, 16); //cima
+		sprites[1] = Game.spritesheet.getSprite(32, 64, 16, 16); //centro
+		sprites[2] = Game.spritesheet.getSprite(16, 64, 16, 16); //esquerda
+		sprites[3] = Game.spritesheet.getSprite(32, 64, 16, 16); //centro
+		sprites[4] = Game.spritesheet.getSprite(48, 64, 16, 16); //direita
+		sprites[5] = Game.spritesheet.getSprite(32, 64, 16, 16); //centro
+	}
+	
+	public void tick() {
 		frames++;
 		if(frames == maxFrames) {
 			frames = 0;
@@ -27,9 +33,10 @@ public class Flower extends Entity{
 				index = 0;
 			}
 		}
+		
 	}
 	
-	public void render(Graphics g) { //trocar o tamanho da mascara (para colidirem com o tamanho correto)
+	public void render(Graphics g) {
 		g.drawImage(sprites[index], this.getX() - Camera.x,this.getY() - Camera.y,null);
 	}
 }
---
+++ src/com/starton/entities/teste.java
@@ -0,0 +1,5 @@
+package com.starton.entities;
+
+public class teste {
+
+}
